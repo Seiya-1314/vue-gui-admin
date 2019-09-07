5f25a60e296328dbfacf4654321208822774b54f
seiya <seiya.Zhufan@gmail.com>
2019-09-07 11:24:03 +0800

    fix: lint

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,4 +1,10 @@
+/**
+ * @description 让 ts 识别 .vue
+ *
+ * 告诉 TypeScript *.vue 后缀的文件可以交给 vue 模块来处理
+ */
+
 declare module '*.vue' {
-  import Vue from 'vue'
-  export default Vue
+  import Vue from 'vue';
+  export default Vue;
 }
