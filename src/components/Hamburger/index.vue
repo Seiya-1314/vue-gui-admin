367d7e26b1aa2b6f18c478607611982c35093d4d
seiya <seiya.Zhufan@gmail.com>
2019-09-11 15:53:03 +0800

    feat: 新增hamburger组件

---
+++ src/components/Hamburger/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    :class="[{'is-active': isActive}]"
+    @click="handleClick"
+  >
+    <svg-icon icon-class="hamburger" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component({
+  name: 'hamburger'
+})
+export default class extends Vue {
+  @Prop({ default: false })
+  private isActive!: boolean
+
+  private handleClick() {
+    this.$emit('toggleClick')
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.svg-icon {
+  vertical-align: middle;
+}
+.is-active {
+  transform: rotate(180deg);
+}
+</style>
