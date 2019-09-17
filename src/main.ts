a0c5d3a77d877acbd9d0153f9593b759a8b73f8d
seiya <seiya.Zhufan@gmail.com>
2019-09-17 21:56:24 +0800

    feat: 新增全局指令

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
-import Vue from 'vue';
+import Vue, { DirectiveOptions } from 'vue';
 
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
-import './plugins'
-import './icons' // icon
+import './plugins';
+import './icons';
 
-import '@/styles/index.scss'
+import '@/styles/index.scss';
+
+import * as directives from '@/directives'
+
+// Register global directives
+Object.keys(directives).forEach(key => {
+  Vue.directive(key, (directives as { [key: string ]: DirectiveOptions })[key])
+})
 
 Vue.config.productionTip = false;
 
