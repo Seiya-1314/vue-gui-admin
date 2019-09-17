706e630fc71edc9035306059a14835dc8cd8f358
seiya <seiya.Zhufan@gmail.com>
2019-09-17 18:40:38 +0800

    update

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,9 +1,10 @@
-import Vue from 'vue'
-import SvgIcon from '@/components/SvgIcon/index.vue'// svg component
+import Vue from 'vue';
+import SvgIcon from '@/components/SvgIcon/index.vue'; // svg component
 
-Vue.component('svg-icon', SvgIcon)
+Vue.component('svg-icon', SvgIcon);
 
-const req = require.context('@/icons/svg/', false, /\.svg$/)
-const requireAll = (requireContext: any) => requireContext.keys().map(requireContext)
+const req = require.context('@/icons/svg/', false, /\.svg$/);
+const requireAll = (requireContext: any) =>
+  requireContext.keys().map(requireContext);
 
-requireAll(req)
+requireAll(req);
