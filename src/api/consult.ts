bdd16369a6570e461eccf0ebc7e84757f0256834
seiya <seiya.Zhufan@gmail.com>
2019-09-18 17:04:36 +0800

    update

--- src/api/consult.ts
+++ src/api/consult.ts
@@ -1,12 +1,54 @@
 import request from '@/utils/request';
 
-const consultService = {
-  getConsultList() {
-    return request({
-      url: '/api/consult',
-      method: 'get'
-    })
-  }
+export interface ConsultQueryConfig {
+  id: string;
+  userID: string;
+  phone: string;
+  region: string;
+  caseType: CaseTypeOption | string;
+  status: StatusTypeOption | string;
+}
+
+export enum CaseTypeOption {
+  ESTATE = '房地产',
+  MARRIAGE = '婚姻家庭',
+  INVESTMENT = '投资理财',
+  OWING = '债权债务',
+  ACCIDENT = '交通事故',
+  BUSINESS = '商业'
+}
+
+export enum StatusTypeOption {
+  REPLY = '已回复',
+  UNANSWERED = '未回复'
+}
+
+export interface ConsultTableConfig {
+  id: string;
+  phone: string;
+  caseType: string;
+  region: string;
+  answer?: string;
+  respondent: string;
+  replyTime: string;
+  issue: string;
+  issueTime: string;
+  status: string;
+  option?: string;
+}
+
+export const getConsultList = (data: any) => {
+  return request({
+    url: '/api/consult',
+    method: 'get',
+    data
+  });
 };
 
-export default consultService;
+export const searchConsult = (data: ConsultQueryConfig) => {
+  return request({
+    url: '/api/consult',
+    method: 'post',
+    data
+  });
+};
